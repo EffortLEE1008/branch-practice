d0dc766938fab9c69849dba66bee9bb368ce9b98
LazyLEE1008 <66935871+LazyLEE1008@users.noreply.github.com>
2024-01-15 14:40:13 +0900

    error

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+def fizz(num):
+    for i in range(num):
+        if i%3==0:
+            print('fizz')
+        elif i%5==0:
+            print('buzz')
+
+
+
+
+if __name__=='__main__':
+    fizz(16)
+        
+
+
+
+
+
